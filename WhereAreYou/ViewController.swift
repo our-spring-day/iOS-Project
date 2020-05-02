15260587fd29e9547ec9f0133710164c97483ef8
s-doris <swdoriz@gmail.com>
2020-05-02 15:33:17 +0900

    Initial Project :)

---
+++ WhereAreYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereAreYou
+//
+//  Created by once on 2020/05/02.
+//  Copyright © 2020 once. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
